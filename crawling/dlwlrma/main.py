39b8913c92afd763f29ed7b6307378d02dae825e
cand0 <rudtjr7655@gmail.com>
2019-12-31 06:29:51 +0000

    user.name to user.id

--- crawling/dlwlrma/main.py
+++ crawling/dlwlrma/main.py
@@ -3,7 +3,17 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 
-req = requests.get('https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={"id":"1692800026","first":1000,"after":"QVFCaVBhUGtHNDI5XzRBMlczeHh1TVBZVHRqS2hBUkpDazJjanBna1IybHd0N2pJUXNwdWN0UmZCNktNTmRoSUktNFBmYmlwVGZuOUZzci1tRFNNd2NZUA=="}')
+#user_id = 1692800026
+user_name = input('ID 를 입력하세요 :')
+user_name_req = requests.get('https://www.instagram.com/' + user_name + '?__a=1')
+user_name_raw = user_name_req.text
+
+pattern = 'profilePage_(.*?)\"'
+pat_res = re.compile(pattern)
+user_id = pat_res.findall(user_name_raw)
+
+
+req = requests.get('https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={"id":"' + str(user_id[0]) + '","first":1000,"after":"QVFCaVBhUGtHNDI5XzRBMlczeHh1TVBZVHRqS2hBUkpDazJjanBna1IybHd0N2pJUXNwdWN0UmZCNktNTmRoSUktNFBmYmlwVGZuOUZzci1tRFNNd2NZUA=="}')
 raw = req.text
 
 html = str(BeautifulSoup(raw, 'html.parser'))	#str로 형변환을 해줘야 수월하게 진행이 된다.
@@ -15,7 +25,7 @@ url_res = pat_res.findall(html_res)
 
 print('Beginning file download...')
 
-for i in range(1, 300):
+for i in range(1, 100):
 	urllib.request.urlretrieve(url_res[i], "./imagefile/test" + str(i) + ".png")
 
 

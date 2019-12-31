5083c3a418b66adde2658721577e08965428cbd0
cand0 <rudtjr7655@gmail.com>
2019-12-31 05:22:10 +0000

    insufficient IU crawling

--- crawling/dlwlrma/dlwlrma.md
+++ crawling/dlwlrma/dlwlrma.md
@@ -1,3 +1,4 @@
 분석 보고서를 참고하여 crawling 진행.
+
 분석 보고서 URL : https://hackmd.io/@H9a0IrQ5QL6VVWhA0jE8_Q/Sk8AOCpCr
 
--- crawling/dlwlrma/main.py
+++ crawling/dlwlrma/main.py
@@ -1,20 +1,24 @@
 import requests
+import urllib.request
 import re
 from bs4 import BeautifulSoup
 
-#str로 형변환을 해줘야 수월하게 진행이 된다.
-
-req = requests.get('https://www.instagram.com/dlwlrma/?__a=1')
+req = requests.get('https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={"id":"1692800026","first":1000,"after":"QVFCaVBhUGtHNDI5XzRBMlczeHh1TVBZVHRqS2hBUkpDazJjanBna1IybHd0N2pJUXNwdWN0UmZCNktNTmRoSUktNFBmYmlwVGZuOUZzci1tRFNNd2NZUA=="}')
 raw = req.text
 
-html = str(BeautifulSoup(raw, 'html.parser'))
+html = str(BeautifulSoup(raw, 'html.parser'))	#str로 형변환을 해줘야 수월하게 진행이 된다.
 html_res = html.replace('&amp;', '&')
 
 pattern = 'display_url":"(.*?)\",'
 pat_res = re.compile(pattern)
-url_res = str(pat_res.findall(html_res))
+url_res = pat_res.findall(html_res)
+
+print('Beginning file download...')
+
+for i in range(1, 300):
+	urllib.request.urlretrieve(url_res[i], "./imagefile/test" + str(i) + ".png")
 
 
-fp = open('dlwlrma_url', 'w')
-fp.write(url_res)
-fp.close()
+#fp = open('dlwlrma_url', 'w')
+#fp.write(str(url_res))
+#fp.close()

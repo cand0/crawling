bada488e322e2153997a973ec7b3c92d61303dca
cand0 <rudtjr7655@gmail.com>
2020-02-01 05:16:13 +0000

    Code that runs but needs to be fixed

--- crawling/main.py
+++ crawling/main.py
@@ -48,17 +48,47 @@ async def Profile():
 		vid_res = list(set(vid_res))
 	print("Downloading...")
 	for i in range(len(pic_res)):
-		await loop.run_in_executor(None, urllib.request.urlretrieve, pic_res[i], "./File/" + user_name + "/picture/" + str(user_name) + str(i + 1) + ".jpeg")
+		await loop.run_in_executor(None, urllib.request.urlretrieve, pic_res[i], "./File/profile/" + user_name + "/picture/" + str(user_name) + str(i + 1) + ".jpeg")
 	for i in range(len(vid_res)):
-		await loop.run_in_executor(None, urllib.request.urlretrieve, vid_res[i], "./File/" + user_name + "/video/" + str(user_name) + str(i + 1) + ".mp4")
+		await loop.run_in_executor(None, urllib.request.urlretrieve, vid_res[i], "./File/profile/" + user_name + "/video/" + str(user_name) + str(i + 1) + ".mp4")
 
 	FileSetting(2, user_name)
 
+async def Hashtag():
+	print(loop)
+	hashtag = GetRequest(2)
+	request_post = HRequestPost()
+
+	shortcode = await GetScode(loop, hashtag, request_post)
+	print(shortcode)
+	for j in range(0, len(shortcode)):
+		url = 'https://www.instagram.com/p/' + shortcode[j] + '/?__a=1'
+		req = await loop.run_in_executor(None, requests.get, url)
+#		req = await requests.get(url)
+		raw = req.text
+		pic_url = PatExt(1, "pic_pattern", raw)
+		vid_url = PatExt(1, "vid_pattern", raw)
+		await loop.run_in_executor(None, urllib.request.urlretrieve, pic_url[0], "./File/hashtag/" + hashtag + "/picture/" + str(hashtag) + str(j + 1) + ".jpeg")
+#		await urllib.request.urlretrieve(pic_url[0], "./File/" + hashtag + "/picutre/" + str(hashtag) + str(j + 1) + ".jpeg")
+
+		if vid_url == []:
+			continue
+		await loop.run_in_executor(None, urllib.request.urlretrieve, vid_url[0], "./File/hashtag/" + hashtag + "/video/" + str(hashtag) + str(j + 1) + ".mp4")
+
 
 async def main():
-	download_tasks = []
-	download_tasks.append(asyncio.ensure_future(Profile()))
-	await asyncio.gather(*download_tasks)
+
+	kinds = int(input('\n\n\tprofile은 1을, \n\thashtag는 2를 입력해주세요\n\tinput:'))
+	if kinds == 1:
+		download_tasks = []
+		download_tasks.append(asyncio.ensure_future(Profile()))
+		await asyncio.gather(*download_tasks)
+	elif kinds == 2:
+		download_tasks = []
+		download_tasks.append(asyncio.ensure_future(Hashtag()))
+		await asyncio.gather(*download_tasks)
+	else :
+		print("\n\tError!!!")
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
